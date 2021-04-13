cc3a7d395e9cfd8e96fabf3e9b8dce204e499084
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-04-13 18:32:43 -0400

    MongoDb connected

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
+const {key} = require('./info');
+
 const app = express();
+
+mongoose.connect(key, { useNewUrlParser: true , useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.set('useFindAndModify', false);
 
 app.use(logger('dev'));
 app.use(express.json());
