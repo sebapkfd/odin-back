932abb018752f247c0a81fef04fa5b497885d6ac
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-04-15 16:30:44 -0400

    Added post, get and get all comments routes

---
+++ controllers/commentController.js
@@ -0,0 +1,33 @@
+const Comment = require('../models/comment');
+
+exports.createComment = (req, res, next) => {
+    const newComment = new Comment({
+        text: req.body.text,
+        user: req.body.user,
+        post: req.body.post
+    })
+    newComment.save((err) => {
+        if (err) { return next(err)}
+        res.status(200).json({msg: 'Comment sucessfully added'})
+    })
+}
+
+exports.getAllComments = (req, res, next) => {
+    Comment.find({})
+    .exec((err, result) => {
+        if (err) { return next(err)}
+        else{
+            res.status(200).json(result)
+        }
+    })
+}
+
+exports.getCommentDetail = (req, res, next) => {
+    Comment.findById(req.params.id)
+    .exec((err, result) => {
+        if (err) { return next(err)}
+        else{
+            res.status(200).json(result)
+        }
+    })
+}
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const userController = require('../controllers/userController');
 const postController = require('../controllers/postController');
+const commentController = require('../controllers/commentController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -26,5 +27,11 @@ router.get('/posts/:id', postController.getPostDetail);
 router.delete('/posts/:id', postController.deletePost);
 
 router.put('/posts/edit/:id', postController.editPost);
+
+router.post('/comments', commentController.createComment);
+
+router.get('/comments', commentController.getAllComments);
+
+router.get('/comments/:id', commentController.getCommentDetail);
 
 module.exports = router;
