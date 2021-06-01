aa49d567a1a48bf1f08b1f8d4609fff48b172d93
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-06-01 16:46:51 -0400

    Modified password max length

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const UserSchema = new Schema({
     firstName: {type: String, required: true, maxlength: 20, minlength: 1},
     lastName: {type: String, required: true, maxlength: 20, minlength: 1},
     email: {type: String, required: true},
-    password: {type: String, required: true, maxlength: 20, minlength: 1},
+    password: {type: String, required: true, minlength: 1},
     friendList: [{type: Schema.Types.ObjectId, ref: "User"}],
     friendRequests: [{type: Schema.Types.ObjectId, ref: "User"}],
 })
