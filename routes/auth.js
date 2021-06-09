e6164361bb25d56107238665b9643f487a37eee4
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-06-09 18:25:56 -0400

    Removed unused code lines

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-// const passport = require('../passport');
-const passport = require('passport');
 
 const userController = require('../controllers/userController');
 
